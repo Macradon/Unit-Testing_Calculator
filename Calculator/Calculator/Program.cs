a7466eb0397c798eab24bb4350695d96d9ef8bb0
Macradon <36033419+Macradon@users.noreply.github.com>
2019-01-30 11:33:25 +0100

    Exercise 1 - Calculator
    Klassen Calculator er implementeret, samt er der blevet implementeret en test-klasse som tager parametre for at kunne teste Calculator-klassens metoder.

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 5;
+            double b = 10;
+
+            Test.test(a, b);
+        }
+    }
+}
---
+++ Calculator/Calculator/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Test
+    {
+        public static void test(double a, double b)
+        {
+            Console.WriteLine(Calculator.Add(a, b));
+            Console.WriteLine(Calculator.Subtract(a, b));
+            Console.WriteLine(Calculator.Multiply(a, b));
+            Console.WriteLine(Calculator.Power(a, b));
+        }
+    }
+}
