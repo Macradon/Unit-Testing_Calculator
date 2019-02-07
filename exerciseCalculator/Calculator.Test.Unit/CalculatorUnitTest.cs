a5039b00d2e3d37aacf9faa19003c1acd2894fe3
MortenBech <mortenbech22@hotmail.com>
2019-02-07 11:45:40 +0100

    Tilføjet Calculator's overload funktioner. Tilføjet simple Testcases. Tilføjet Clear funktion. Tilføjet Accumulator.

---
+++ exerciseCalculator/Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test.Unit
+{
+
+    [TestFixture]
+    public class CalculatorUnitTest
+    {
+
+        private Calculator uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Calculator();
+        }
+
+        [TestCase(2, 4, 6)]
+        [TestCase(3, 6, 9)]
+        [TestCase(14, 18, 32)]
+        public void Add_AddPositiveNumbers(double a, double b, double result)
+        {
+            Assert.That(uut.Add(a, b), Is.EqualTo(result));
+        }
+
+        [TestCase(-2, -4, -6)]
+        [TestCase(-3, -6, -9)]
+        [TestCase(-14, -18, -32)]
+        public void Add_AddNegativeNumbers(double a, double b, double result)
+        {
+            Assert.That(uut.Add(a, b), Is.EqualTo(result));
+        }
+
+        [TestCase(2, -4, -2)]
+        [TestCase(-3, 6, 3)]
+        [TestCase(-17, 17, 0)]
+        public void Add_AddPosAndNegNumbers(double a, double b, double result)
+        {
+            Assert.That(uut.Add(a, b), Is.EqualTo(result));
+        }
+
+        [TestCase(7, 4, 3)]
+        [TestCase(6, 12, -6)]
+        [TestCase(14, 14, 0)]
+        public void Add_SubtractPositiveNumbers(double a, double b, double result)
+        {
+            Assert.That(uut.Subtract(a, b), Is.EqualTo(result));
+        }
+
+        [TestCase(-7, -4, -3)]
+        [TestCase(-6, -12, 6)]
+        [TestCase(-14, -14, -0)]
+        public void Add_SubtractNegativeNumbers(double a, double b, double result)
+        {
+            Assert.That(uut.Subtract(a, b), Is.EqualTo(result));
+        }
+
+        [TestCase(-7, -4, -3)]
+        [TestCase(-6, -12, 6)]
+        [TestCase(-14, -14, -0)]
+        public void Add_SubtractPosAndNegNumbers(double a, double b, double result)
+        {
+            Assert.That(uut.Subtract(a, b), Is.EqualTo(result));
+        }
+    }
+}
---
+++ exerciseCalculator/Calculator/Calculator.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            double value = 1;
+            for (int i = 0; i < exp; i++)
+            {
+                value = value * x;
+            }
+
+            return value;
+        }
+
+
+
+        public double Add(double addend)
+        {
+            result = Accumulator + addend;
+            return result;
+        }
+
+        public double Subtract(double subtractor)
+        {
+            result = Accumulator + subtractor;
+            return result;
+        }
+
+        public double Multiply(double multiplier)
+        {
+            result = Accumulator + multiplier;
+            return result;
+        }
+
+        public double Divide(double divisor)
+        {
+            result = Accumulator + divisor;
+            return result;
+        }
+
+        public double Power(double exponent)
+        {
+            for (int i = 0; i < exponent - 1; i++)
+            {
+                result = result * result;
+            }
+            result = Accumulator + exponent;
+            return result;
+        }
+
+
+
+        public void Clear()
+        {
+            result = 0;
+        }
+
+        private double result = 0;
+
+        public double Accumulator
+        {
+            get
+            {
+                return result;
+            }
+            private set
+            {
+                result = value;
+            }
+        }
+    }
+}
